9617a3e7fa79b9087b73845371d840964e0a0080
Nikhil Kumawat <143185335+kumawatnikhil42@users.noreply.github.com>
2024-08-28 17:13:25 +0530

    SQL

--- Day 6.sql
+++ Day 6.sql
@@ -28,12 +28,11 @@ group by p.project_id
 
 
 
-SELECT product_id, 
-       product_name 
-FROM   product 
-WHERE  product_id NOT IN (SELECT product_id 
-                          FROM   sales 
-                          WHERE  sale_date NOT BETWEEN 
-                                 '2019-01-01' AND '2019-03-31'); 
+select p.product_id,p.product_name 
+from Product as p
+join Sales as s on (p.product_id=s.product_id)
+group by p.product_id
+having min(s.sale_date) >= '2019-01-01' 
+    and max(s.sale_date) <= '2019-03-31'; 
 
 
